aceb39696d6ee6727eab354daa155240a87b443c
Preetha2402 <preethanjali.d@gmail.com>
2024-11-30 13:20:44 +0530

    Update WebElement.java

--- assignmen3/Assignment3/WebElement.java
+++ assignmen3/Assignment3/WebElement.java
@@ -1,2 +1,8 @@
 package Assignment3;
 
+	public void click() {
+		System.out.println("Click on Webelement");
+	}
+	
+	public void setText(String text) {
+	System.out.println("Settext in Webelement is : " + text);
